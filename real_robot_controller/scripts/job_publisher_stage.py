af9fee439de3440a1f68c36097a3ef5f1ecc7222
cch-cs <cch-cs@ipa.fhg.de>
2018-12-21 12:55:01 +0100

    changed the jobs

--- real_robot_controller/scripts/job_publisher_stage.py
+++ real_robot_controller/scripts/job_publisher_stage.py
@@ -9,12 +9,12 @@ def job():
     pub = rospy.Publisher('job', Job, queue_size=1)
     job = Job()
     job.job_start.header.frame_id = "map"
-    job.job_start.pose.position.x = 4 # 10.5
+    job.job_start.pose.position.x = 6 # 10.5
     job.job_start.pose.position.y = 2 # 12.2
     job.job_start.pose.orientation.w = 1
     job.job_goal.header.frame_id = "map"
-    job.job_goal.pose.position.x = 1 # 11.4
-    job.job_goal.pose.position.y = 4 # 12.7
+    job.job_goal.pose.position.x = 2 # 11.4
+    job.job_goal.pose.position.y = 3 # 12.7
     job.job_goal.pose.orientation.w = 1
     job.job_time = 1
     while True:
@@ -25,10 +25,10 @@ def job():
     job = Job()
     job.job_start.header.frame_id = "map"
     job.job_start.pose.position.x = 6 # 10.5
-    job.job_start.pose.position.y = 2 # 12.2
+    job.job_start.pose.position.y = 4 # 12.2
     job.job_start.pose.orientation.w = 1
     job.job_goal.header.frame_id = "map"
-    job.job_goal.pose.position.x = 7 # 11.4
+    job.job_goal.pose.position.x = 5 # 11.4
     job.job_goal.pose.position.y = 2 # 12.7
     job.job_goal.pose.orientation.w = 1
     job.job_time = 2
